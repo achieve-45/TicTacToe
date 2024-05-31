b3350904f8c7bc5d3dca1d4922adbc53141fff64
achieve-45 <pupuachieve7@gmail.com>
2024-05-31 17:46:37 +0530

    added packages

--- src/LaunchGame.java
+++ src/LaunchGame.java
@@ -1,3 +1,5 @@
+package src;
+
 public class LaunchGame {
     public static void main(String[] args) {
         TicTacToe t = new TicTacToe();
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,5 @@
+package src;
+
 public class TicTacToe {
     static char[][] board;
 
